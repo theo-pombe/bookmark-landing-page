5392746eeaa7c6378717a9139ebb171d3256c297
theo-pombe <theo.pombe@gmail.com>
2023-05-13 16:31:53 +0300

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./*.html", "./dist/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        collapse: "collapse 250ms ease-in-out",
+        fade: "fade-in 2s ease-in-out",
+        "fade-in": "fade-in 1s ease-in",
+        "fade-out": "fade-out 1s ease-out",
+      },
+      keyframes: {
+        collapse: {
+          "0%": { opacity: 0.5 },
+          "50%": { opacity: 0.75 },
+          "100%": { opacity: 1 },
+        },
+        fade: {
+          "0%": { opacity: 0.75 },
+          "100%": { opacity: 1 },
+        },
+        "fade-in": {
+          "0%": { opacity: 0.5 },
+          "100%": { opacity: 1 },
+        },
+        "fade-out": {
+          "0%": { opacity: 0.5, transform: "translate(-75px)" },
+          "100%": { opacity: 1, transform: "translate(0)" },
+        },
+      },
+    },
     fontFamily: { Rubik: ["Rubik, sans-serif"] },
     container: {
       center: true,
